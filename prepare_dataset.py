e16e38b180708ab675ba64f5e13c55f4a22184d1
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-09-28 17:33:44 +0700

    load data

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -15,3 +15,26 @@ def prepare_dataset(dataset_path, json_path, n_mfcc=13, hop_length=512, n_fft=20
         "MFCCs": [],
         "files": []
     }
+
+    # loop thorugh all the sub-dirs
+    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+
+        # ensure not at the root level
+        if dirpath is not dataset_path:
+
+            # update mappings
+            category = dirpath.split("/")[-1] # dataset/down -> [dataset, down]
+            data["mappings"].append(category)
+
+            # loop through all the filenames and extract MFCCs
+            for f in filenames:
+
+                # get file path
+                file_path = os.path.join(dirpath, f)
+
+                # load audio file
+                signal, sr = librosa.load(file_path)
+
+                # ensure the audio file is at least 1 sec
+                if len(signal) >= SAMPLES_TO_CONSIDER:
+                    pass
