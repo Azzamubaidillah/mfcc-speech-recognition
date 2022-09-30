d1a737186645827f48b793c613b88d15c1a597f8
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-09-30 11:20:02 +0700

    train model, evaluate, and save

--- train.py
+++ train.py
@@ -1,7 +1,10 @@
 
 DATA_PATH = "data.json"
+SAVED_MODEL_PATH = "model.h5"
 
 LEARNING_RATE = 0.0001
+EPOCHS = 40
+BATCH_SIZE = 32
 
 def main():
     # load train/validation/test data splits
@@ -12,9 +15,12 @@ def main():
     model = build_model(input_shape, LEARNING_RATE)
 
     # train the model
-
+    model.fit(X_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE,
+              validation_data=(X_validation, y_validation))
 
     # evaluate the model
-
+    test_error, test_accuracy = model.evaluate(X_test, y_test)
+    print(f"Test error: {test_error}, test accuracy: {test_accuracy}")
 
     # save the model
+    model.save(SAVED_MODEL_PATH)
